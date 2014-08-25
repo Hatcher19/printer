3e81b4c05899f80b75cd64bf76e308c632fe7ab8
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2014-08-24 20:47:06 -0400

    heroku fix migrations

--- db/migrate/20140508140119_remove_plan_id_and_stripe_card_token_and_stripe_customer_token_and_email_from_accounts.rb
+++
@@ -1,8 +0,0 @@
-class RemovePlanIdAndStripeCardTokenAndStripeCustomerTokenAndEmailFromAccounts < ActiveRecord::Migration
-  def change
-    remove_column :accounts, :plan_id, :integer
-    remove_column :accounts, :stripe_card_token, :string
-    remove_column :accounts, :stripe_customer_token, :string
-    remove_column :accounts, :email, :string
-  end
-end
