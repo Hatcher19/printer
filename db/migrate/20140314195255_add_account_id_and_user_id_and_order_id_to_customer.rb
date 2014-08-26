e744d1f2002a0465e1dd1e7aa0749f05fd459d83
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2014-08-25 21:32:33 -0400

    heroku fix

--- db/migrate/20140314195255_add_account_id_and_user_id_and_order_id_to_customer.rb
+++
@@ -1,7 +0,0 @@
-class AddAccountIdAndUserIdAndOrderIdToCustomer < ActiveRecord::Migration
-  def change
-    add_column :customers, :account_id, :integer
-    add_column :customers, :user_id, :integer
-    add_column :customers, :order_id, :integer
-  end
-end
