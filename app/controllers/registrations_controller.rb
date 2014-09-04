c0e2482f7fe5b7facb617b5054820251f44c73ca
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2014-09-03 22:53:35 -0400

    test bit bucket

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
   skip_before_filter :require_no_authentication 
-
-  # def after_sign_up_path_for(resource)
-  #   flash[:analytics] = "/goals/signup"
-  #   new_ga_service_path
-  # end
 end
